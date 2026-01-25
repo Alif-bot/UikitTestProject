65d6852d7fff24d03c11641d99a048d57a7ec546
Md Alif Hossain <infoalif7@gmail.com>
2026-01-25 16:28:57 +0600

    Code refactor

--- Files/Model/VocalModel.swift
+++ Files/Model/VocalModel.swift
@@ -15,14 +15,50 @@ struct VocalModel {
 
 extension VocalModel {
     static var mock: [VocalModel] = [
-        .init(name: "Azek", genre: "hiphop, husky", imageURL: "https://randomuser.me/api/portraits/men/10.jpg"),
-        .init(name: "Elara", genre: "pop, ethereal", imageURL: "https://randomuser.me/api/portraits/women/20.jpg"),
-        .init(name: "Talia", genre: "country, sweet", imageURL: "https://randomuser.me/api/portraits/women/30.jpg"),
-        .init(name: "Bella", genre: "r&b, powerful", imageURL: "https://randomuser.me/api/portraits/women/40.jpg"),
-        .init(name: "Gia", genre: "electronic, powerful", imageURL: "https://randomuser.me/api/portraits/women/50.jpg"),
-        .init(name: "Bruce", genre: "r&b, silky", imageURL: "https://randomuser.me/api/portraits/men/60.jpg"),
-        .init(name: "James", genre: "pop, captivating", imageURL: "https://randomuser.me/api/portraits/men/70.jpg"),
-        .init(name: "Stan", genre: "pop", imageURL: "https://randomuser.me/api/portraits/men/80.jpg"),
-        .init(name: "Rodrick", genre: "r&b", imageURL: "https://randomuser.me/api/portraits/men/90.jpg")
+        .init(
+            name: "Azek",
+            genre: "hiphop, husky",
+            imageURL: "https://randomuser.me/api/portraits/men/10.jpg"
+        ),
+        .init(
+            name: "Elara",
+            genre: "pop, ethereal",
+            imageURL: "https://randomuser.me/api/portraits/women/20.jpg"
+        ),
+        .init(
+            name: "Talia",
+            genre: "country, sweet",
+            imageURL: "https://randomuser.me/api/portraits/women/30.jpg"
+        ),
+        .init(
+            name: "Bella",
+            genre: "r&b, powerful",
+            imageURL: "https://randomuser.me/api/portraits/women/40.jpg"
+        ),
+        .init(
+            name: "Gia",
+            genre: "electronic, powerful",
+            imageURL: "https://randomuser.me/api/portraits/women/50.jpg"
+        ),
+        .init(
+            name: "Bruce",
+            genre: "r&b, silky",
+            imageURL: "https://randomuser.me/api/portraits/men/60.jpg"
+        ),
+        .init(
+            name: "James",
+            genre: "pop, captivating",
+            imageURL: "https://randomuser.me/api/portraits/men/70.jpg"
+        ),
+        .init(
+            name: "Stan",
+            genre: "pop",
+            imageURL: "https://randomuser.me/api/portraits/men/80.jpg"
+        ),
+        .init(
+            name: "Rodrick",
+            genre: "r&b",
+            imageURL: "https://randomuser.me/api/portraits/men/90.jpg"
+        )
     ]
 }
